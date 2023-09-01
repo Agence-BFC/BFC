8275086cc3602869b4c7fd071c7dbf588ee0b41e
flo611 <florian.sabine911@outlook.fr>
2023-09-01 22:56:20 +0200

    update

--- src/app/home/section/introduction.jsx
+++ src/app/home/section/introduction.jsx
@@ -5,9 +5,9 @@ const Introduction = () => {
     <>
       <section className=" pt-16  flex flex-col lg:flex lg:flex-row lg:pt-32 ">
         <div className="pt-10 contain my-10 mx-6 ">
-          <div className="flex flex-row justify-center ">
+          <div className="flex flex-row justify-center animate-pulse ">
             <Image src="/images/chloe.png" width={80} height={80} alt="chloe" />
-            <div className="px-1">
+            <div className="px-1 ">
               <Image
                 src="/images/bollo.png"
                 width={80}
@@ -50,7 +50,7 @@ const Introduction = () => {
                 width={300}
                 height={200}
                 alt="logo"
-                className="card my-4  mx-6  lg:my-96 "
+                className="card my-4  mx-6  lg:my-96   "
               />
             </div>
           </a>
--- src/app/home/section/services.jsx
+++ src/app/home/section/services.jsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import "./styles.css";
-import { BsKey,BsPaintBucket,BsPen } from "react-icons/bs";
+import { BsKey, BsPaintBucket, BsPen } from "react-icons/bs";
 
 const Services = () => {
   return (
     <>
-      <h1 className="flex flex-row justify-center py-3 my-3 text-amber-900 text-2xl lg:text-6xl lg:py-10 shadow-2xl shadow-amber-800 mx-20 lg:mx-96 rounded-full">
+      <h1 className="flex flex-row justify-center py-3 my-3 text-amber-900 text-2xl lg:text-6xl lg:py-10 shadow-lg shadow-amber-800/80 mx-20 lg:mx-96 rounded-full hover:bg-gradient-to-r  ">
         Services
       </h1>
       <section className="lg:flex lg:flex-row  " id="services">
@@ -17,7 +17,7 @@ const Services = () => {
               </h2>
               <ul className="list-disc my-6  lg:text-4xl lg:my-6">
                 <li id="Lorem" className="" link href="/Lorem">
-                   Responsive
+                  Responsive
                 </li>
                 <li id="Lorem" className=" py-2 " link href="Lorem">
                   Cahiers des charges
@@ -27,9 +27,9 @@ const Services = () => {
                 </li>
               </ul>
               <div className="text-5xl lg:text-9xl py-2 lg:py-4">
-                  <BsPen width={100} height={100} />
-                </div>
-            
+                <BsPen width={100} height={100} />
+              </div>
+
               <a href="/services">
                 <button
                   className=" flex flex-row justify-center bg-green-800 my-3  rounded-full hover-bg-green-500 uppercase px-3 py-3 w-36 hover:text-white hover:transition hover:ease-in-out hover:duration-200  lg:text-lg lg:w-48 dark:hover:bg-amber-950 "
@@ -44,11 +44,12 @@ const Services = () => {
         </div>
         <div className="flex-auto justify-center  ">
           <div className=" card flex flex-col items-center rounded-md  bg-yellow-500 mx-8 md:mx-52   hover: m-5 -z-10 pb-2  lg:px-6 lg:mx-24  lg:my-6  hover:ease-in-out hover:duration-200 lg:hover:scale-105">
-            <div className=" flex flex-col contain items-center justify-center px-2 m-4  lg:px-12 ">
+            <div className=" flex flex-col contain items-center  px-2 m-4  lg:px-12 ">
               <h2 className="flex w-full justify-center pt-2  mx-4 text-amber-900 text-xl lg:px-8 lg:mx-20 lg:text-4xl lg:items-center ">
                 Design graphisme
               </h2>
-              <ul className="list-disc my-6 lg:text-4xl lg:my-6">
+              
+              <ul className="list-disc  my-6 lg:text-4xl lg:my-6 ">
                 <li id="Lorem" className="" link href="/Lorem">
                   Maquettage
                 </li>
@@ -60,10 +61,9 @@ const Services = () => {
                 </li>
               </ul>
               <div>
-              <div className="text-5xl lg:text-9xl py-1 lg:py-4">
+                <div className="text-5xl lg:text-9xl py-1 lg:py-4">
                   <BsPaintBucket width={100} height={100} />
                 </div>
-             
               </div>
               <a href="/services">
                 <button
@@ -83,8 +83,8 @@ const Services = () => {
               <h2 className="flex w-full justify-center pt-2  mx-5 text-amber-900 text-xl lg:px-10 lg:mx-20 lg:text-4xl lg:items-center ">
                 Maintenance site
               </h2>
-             
-              <ul className="list-disc my-6 lg:text-4xl lg:my-6">
+
+              <ul className="list-disc w-1/2 my-6 lg:text-4xl lg:my-6">
                 <li id="Lorem" className="" link href="/Lorem">
                   Débogage
                 </li>
@@ -95,20 +95,20 @@ const Services = () => {
                   Sécurité
                 </li>
               </ul>
-                <div className="text-5xl lg:text-9xl lg:py-4">
-                  <BsKey width={120} height={120} />
-                </div>
-              
-             
-              <a href="/services">
-                <button
-                  className=" flex flex-row justify-center bg-green-800 my-3  rounded-full hover-bg-green-500 uppercase px-3 py-3 w-36 hover:text-white hover:transition hover:ease-in-out hover:duration-200  lg:text-lg lg:w-48 dark:hover:bg-amber-950 "
-                  type="button"
-                  value="En savoir +"
-                >
-                  En savoir +
-                </button>
-              </a>
+            
+            <div className="text-5xl lg:text-9xl lg:py-4">
+              <BsKey width={120} height={120} />
+            </div>
+
+            <a href="/services">
+              <button
+                className=" flex flex-row justify-center bg-green-800 my-3  rounded-full hover-bg-green-500 uppercase px-3 py-3 w-36 hover:text-white hover:transition hover:ease-in-out hover:duration-200  lg:text-lg lg:w-48 dark:hover:bg-amber-950 "
+                type="button"
+                value="En savoir +"
+              >
+                En savoir +
+              </button>
+            </a>
             </div>
           </div>
         </div>
