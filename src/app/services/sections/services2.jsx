2e7673bd6e24ce97674ce46f7622413e6e82d44e
chloe775 <chloe.dvsb7@gmail.com>
2023-10-04 16:41:09 +0200

    update

--- src/app/services/sections/services2.jsx
+++ src/app/services/sections/services2.jsx
@@ -3,7 +3,9 @@
 import "./styles.css";
 import Image from "next/image";
 import { useState } from "react";
-
+var React = require("react");
+var ReactDOM = require("react-dom");
+var Carousel = require("react-responsive-carousel").Carousel;
 // const Sowtext=()=>{
 //   const [poyoFields, setPoyoFields] = useState({
 //     poyo1: false,
@@ -15,13 +17,21 @@ import { useState } from "react";
 const Serv = () => (
   <>
     <section className="bg-[#BBF7D0]">
+
+      {/* 2EME VERSION QUI POURRAIS CORRESPONDRE AUX ATTENTES DU PROJET DE LA PAGE SERVICES  */}
+
       <div className="flex flex-row pt-24 justify-center items-center mx-6  drop-shadow-xl lg:grid lg:grid-cols-3 lg:items-center">
-        <button
-          className=" shadow-lg shadow-amber-800/80 border-2 rounded-full px-2 border-amber-800"
-          id="d1"
-        >
-          Création
-        </button>
+        <div>
+          <a href="/contact">
+            <button
+              className=" shadow-lg shadow-amber-800/80 border-2 rounded-full px-2 border-amber-800"
+              id="d1"
+            >
+              Création
+            </button>
+          </a>
+        </div>
+
         <div className="px-6">
           <button
             className=" shadow-lg shadow-amber-800/80 border-2 rounded-full px-2 border-amber-800 "
@@ -35,12 +45,12 @@ const Serv = () => (
         </button>
       </div>
 
-      <div className=" my-6 card flex flex-col items-center rounded-md  opacity-90 bg-[#EEC23980] pt-4 mx-8 md:mx-52    -z-10 pb-2   lg:px-10 lg:mx-16  lg:my-6  ">
-        <h1 className="text-xl">CRÉATION WEB</h1>
-        <div className="w-full grid grid-cols-3 items-center text-center pt-8 lg:grid lg:grid-cols-3 lg:items-center underline">
-          <h1>RESPONSIVE</h1>
-          <h1>CAHIER DES CHARGES</h1>
-          <h1>RESPECT WWW</h1>
+      <div className=" my-6 card flex flex-col items-center rounded-md pt-4 opacity-90 bg-[#EEC23980] mx-8 md:mx-52    -z-10 pb-2   lg:px-10 lg:mx-16  lg:my-6  ">
+        <h1 className="text-xl">Crétation de sites</h1>
+        <div>
+          <button className="text-xs flex justify-center pt-8 font-bold">
+            CRÉATION DE SITES WEB
+          </button>
         </div>
         <div className=" flex flex-col contain bg-[#EEC23980] pt-1 items-center justify-center  px-8 m-4 lg:pb-1 lg:px-12  ">
           <Image
@@ -50,37 +60,106 @@ const Serv = () => (
             alt="logoblack"
           ></Image>
           <div id="d1">
-            <p id="p1" className="hide">
-              Découvrez lart de la création web avec nous : Nous transformons
+            <p id="p1" className="hide text-xs">
+              Découvrez l'art de la création web avec nous : Nous transformons
               vos idées en sites web exceptionnels. Notre expertise, votre
               vision. Ensemble, construisons une présence en ligne qui va
               au-delà de vos attentes pour concrétiser vos rêves digitaux.
             </p>
           </div>
-          <div id="d2">
-            <p id="l1">test 2</p>
-          </div>
-          <div>
-            <p>test 3</p>
-          </div>
-
           <h2 className="underline mt-2">tarifs:</h2>
-          <p className="mt-1">à partir de : 20 000€</p>
+          <p className="mt-1 text-xs ">A partir de : 20 000€</p>
+          <div className="lg:flex lg:flex-row">
+            <a href="/contact">
+              <button
+                className=" flex flex-row justify-center my-2 underline lg:bg-green-800 lg:opacity-90 p-1  lg:rounded lg:placeholder:hover-bg-green-500 uppercase  hover:text-white hover:transition hover:ease-in-out hover:duration-200  lg:text-lg lg:w-48 dark:hover:bg-amber-950 "
+                type="button"
+                value="En savoir +"
+              >
+                <h2>Demander nous un devis</h2>
+              </button>
+            </a>
+          </div>
         </div>
+
+        {/* REFONTE  */}
+
+        <div>
+          <div>
+            <button className="text-xs flex justify-center pt-8 font-bold ">
+              REFONTE WEB
+            </button>
+          </div>
+        </div>
+        <div className=" flex flex-col contain bg-[#EEC23980] pt-1 items-center justify-center  px-8 m-4 lg:pb-1 lg:px-12  ">
+          <Image
+            src="/images/logos/logo-black.svg"
+            width={150}
+            height={150}
+            alt="logoblack"
+          ></Image>
+          <div id="d1">
+            <p id="p1" className="hide text-xs">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat
+              autem quod, ipsa sequi illum dolores eligendi. Voluptas deserunt,
+              a optio neque tempore porro perspiciatis, sapiente dolorem quasi
+              recusandae ullam rem!
+            </p>
+          </div>
+          <h2 className="underline mt-2">tarifs:</h2>
+          <p className="mt-1 text-xs ">A partir de : 20 000€</p>
+          <div className="lg:flex lg:flex-row">
+            <a href="/contact">
+              <button
+                className=" flex flex-row justify-center my-2 underline lg:bg-green-800 lg:opacity-90 p-1  lg:rounded lg:placeholder:hover-bg-green-500 uppercase  hover:text-white hover:transition hover:ease-in-out hover:duration-200  lg:text-lg lg:w-48 dark:hover:bg-amber-950 "
+                type="button"
+                value="En savoir +"
+              >
+                <h2>Demander nous un devis</h2>
+              </button>
+            </a>
+          </div>
+        </div>
+
+        {/* MAINTENANCE ET INTEGRATION */}
+
+        <div>
+          <div>
+            <button className="text-xs flex justify-center pt-8 font-bold ">
+              MAINTENANCE ET INTÉGRATION
+            </button>
+          </div>
+        </div>
+        <div className=" flex flex-col contain bg-[#EEC23980] pt-1 items-center justify-center  px-8 m-4 lg:pb-1 lg:px-12  ">
+          <Image
+            src="/images/logos/logo-black.svg"
+            width={150}
+            height={150}
+            alt="logoblack"
+          ></Image>
+          <div id="d1">
+            <p id="p1" className="hide text-xs">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat
+              autem quod, ipsa sequi illum dolores eligendi. Voluptas deserunt,
+              a optio neque tempore porro perspiciatis, sapiente dolorem quasi
+              recusandae ullam rem!
+            </p>
+          </div>
+          <h2 className="underline mt-2">tarifs:</h2>
+          <p className="mt-1 text-xs ">A partir de : 20 000€</p>
         <div className="lg:flex lg:flex-row">
           <a href="/contact">
             <button
-              className=" flex flex-row justify-center bg-green-800 p-2 mt-4 rounded-full hover-bg-green-500 uppercase  hover:text-white hover:transition hover:ease-in-out hover:duration-200  lg:text-lg lg:w-48 dark:hover:bg-amber-950 "
+              className=" flex flex-row justify-center my-2 underline lg:bg-green-800 lg:opacity-90 p-1  lg:rounded lg:placeholder:hover-bg-green-500 uppercase  hover:text-white hover:transition hover:ease-in-out hover:duration-200  lg:text-lg lg:w-48 dark:hover:bg-amber-950 "
               type="button"
               value="En savoir +"
             >
-              Devis
+              <h2>Demander nous un devis </h2>
             </button>
           </a>
         </div>
+        </div>
       </div>
-
-      {/* séparation de la colone */}
     </section>
   </>
 );
